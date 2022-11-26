9807a55508ca1157b0c6ee993e2f798c9054bedb
Binpuki <jpaulvu@gmail.com>
2022-11-26 04:23:33 -0600

    woah angle

--- source/objects/player/Gun.hx
+++ source/objects/player/Gun.hx
@@ -1,5 +1,6 @@
 package objects.player;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 
 class Gun extends FlxSprite
@@ -9,5 +10,12 @@ class Gun extends FlxSprite
         super();
 
         loadGraphic("assets/images/gun.png");
+    }
+
+    override function update(elapsed:Float)
+    {
+        super.update(elapsed);
+
+        angle = Math.atan2(FlxG.mouse.y - (y + (height / 2)), FlxG.mouse.x - (x + (width / 2))) * (180 / Math.PI);
     }
 }
